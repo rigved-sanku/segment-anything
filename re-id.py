3e068f34f24d35b87b75c82409d465ae75582071
Rigved <sankurigved@gmail.com>
2024-06-06 16:33:42 -0500

    Visualising t-sne plots of SAM embeddings

--- re-id.py
+++ re-id.py
@@ -1,6 +1,12 @@
+""" This script is re-id script that uses the SAM model to extract embeddings from images and then visualize them using t-SNE"""
+
+
 import os
 import cv2
 from PIL import Image
+import plotly.express as px
+
+
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,8 +19,15 @@ from onnxruntime.quantization.quantize import quantize_dynamic
 
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
+import umap
 
 
+# Define the SAM class
+# This class is used to load the SAM model and extract image embeddings
+# The class also has methods to export the model to ONNX format, quantize the model and load the model in onnxruntime 
+# Refer : https://colab.research.google.com/github/facebookresearch/segment-anything/blob/main/notebooks/onnx_model_example.ipynb
 class SAM():
     def __init__(self, checkpoint='sam_vit_h_4b8939.pth', model_type='vit_h'):
         self.sam  = sam_model_registry[model_type](checkpoint = checkpoint)
@@ -80,15 +93,24 @@ class SAM():
         return image_embedding
 
 
-""""
+
+"""
+# Evaluate the embeddings of the images in the test_data folder
+# The embeddings are extracted using the SAM model
+
 test_data_path = '/home/user/Activity_Monitoring/test_data'
+all_items = os.listdir(test_data_path)
+#print(type(all_items[0]))
+all_items = sorted(all_items)
+
 print('Opening Data Folder ...')
 sam = SAM()
 sam.set_device()
 
 all_embeddings = {}
 
-for person_folder in os.listdir(test_data_path):
+for person_folder in all_items:
+    #print(type(person_folder))
     person_path = os.path.join(test_data_path, person_folder)
     
     if os.path.isdir(person_path):
@@ -104,20 +126,73 @@ for person_folder in os.listdir(test_data_path):
             print(f'EXtracted image embedding from {image_path} with shape {image_embedding.shape}')
             all_embeddings[image_path] = image_embedding
             
-np.save('/home/user/Activity_Monitoring/test_data/emebeddings.npy', all_embeddings)
+np.save('/home/user/Activity_Monitoring/test_data/emebeddings.npy', all_embeddings, allow_pickle=True)
 """
 
-       
-all_embeddings = np.load('/home/user/Activity_Monitoring/test_data/emebeddings.npy', allow_pickle=True)
+# Load the embeddings and visualize them using t-SNE
+# The embeddings are first flattened and then normalized using StandardScaler
+all_embeddings = np.load('/home/user/Activity_Monitoring/test_data/emebeddings.npy', allow_pickle=True).item()
+flat_embeddings = []
+person_ids = []
+hover_names = []
 
-for path, embedding in all_embeddings.item():
-    print(f'Opening Image Path: {path} with embedding shape {embedding.shape}')
+for path, embedding in all_embeddings.items():
+    #print(f'Opening Image Path: {path} with embedding shape {embedding.shape}')
     embeddings = np.array(embedding)
-    flattened_embeddings = embeddings.reshape(embeddings.shape[0], -1)
-    print(f'Opening Image Path: {path} with embedding shape {flattened_embeddings.shape}')
-    pca = PCA(n_components=50)
-    reduced_emebeddings = pca.fit_transform(flattened_embeddings)
-    print(f'Opening Image Path: {path} with embedding shape {reduced_emebeddings.shape}')
-        
+    flattened_embedding = embeddings.reshape(embeddings.shape[0], -1)
+    #print(f'Opening Image Path: {path} with embedding shape {flattened_embedding.shape}')
+    flat_embeddings.append(flattened_embedding)
+    path_parts = path.split(os.sep)
+    person_ids.append(path_parts[-2])
+    hover_names.append(path_parts[-1])
+    
+    
+flat_embeddings = np.array(flat_embeddings)
+person_ids = np.array(person_ids)
+flat_embeddings = np.reshape(flat_embeddings, (flat_embeddings.shape[0], flat_embeddings.shape[2]))
+print(f'Flat embedding shape {flat_embeddings.shape}')
+print(f'Person Ids {person_ids.shape}')
+
+scaler = StandardScaler()
+flat_normal_embeddings = scaler.fit_transform(flat_embeddings)
+
+"""
+# Perform PCA on the embeddings
+
+pca = PCA(n_components=50)
+reduced_embeddings = pca.fit_transform(flat_normal_embeddings)
+print(f'Redcued embedding shape {reduced_embeddings.shape}')
+np.save('/home/user/Activity_Monitoring/test_data/pca_emebeddings.npy', reduced_embeddings, allow_pickle=True)
+"""
+
+"""
+# t-sne hyper paramater tuning (perplexity)
+perplexity = np.arange(5, 55, 5)
+divergence = []
+for i in perplexity:
+    model = TSNE(n_components=2, init="pca", perplexity=i)
+    reduced = model.fit_transform(reduced_embeddings)
+    divergence.append(model.kl_divergence_)
+    
+fig = px.line(x=perplexity, y=divergence, markers=True)
+fig.update_layout(xaxis_title="Perplexity Values", yaxis_title="Divergence")
+fig.update_traces(line_color="red", line_width=1)
+fig.show()
+"""
+
+# Perform t-SNE on the embeddings
+# The embeddings are first reduced using PCA
+reduced_embeddings = np.load('/home/user/Activity_Monitoring/test_data/pca_emebeddings.npy', allow_pickle=True)
+tsne = TSNE(n_components=2, perplexity=45, random_state=42)
+tsne_embeddings = tsne.fit_transform(reduced_embeddings)
+
+fig = px.scatter(x=tsne_embeddings[:, 0], y=tsne_embeddings[:, 1], color = person_ids, hover_name = hover_names)
+fig.update_layout(
+    title="t-SNE visualization of person re-id dataset",
+    xaxis_title="First t-SNE",
+    yaxis_title="Second t-SNE",
+)
+fig.show()
+print('Done')
     
         
